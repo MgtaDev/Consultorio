6c5209b4cab570a5ec53db7ca79ae21679600534
MgtaDev <passantinodev@gmail.com>
2023-11-07 19:52:15 -0400

    back connect

--- Front-end/src/redux/actions.js
+++ Front-end/src/redux/actions.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 import { GET_ALL_CLIENTES, GET_ALL_MEDICOS, GET_CITA_ID, GET_ALL_CITAS, GET_CITAS_PER_CLIENT } from './action-types'
 // ``````
 export const allClientes = () => async (dispatch) => {
-    const clientes = await axios.get(`http://localhost:3001/cliente`);
+    const clientes = await axios.get(`https://consultorio-production.up.railway.app/cliente`);
     dispatch({
       type: GET_ALL_CLIENTES,
       payload: clientes,
     });
 }
 export const allMedicos = () => async (dispatch) => {
-    const medicos = await axios.get(`http://localhost:3001/medico`);
+    const medicos = await axios.get(`https://consultorio-production.up.railway.app/medico`);
     dispatch({
       type: GET_ALL_MEDICOS,
       payload: medicos,
     });
 }
 export const allCitas = () => async (dispatch) => {
-    const citas = await axios.get(`http://localhost:3001/cita`)
+    const citas = await axios.get(`https://consultorio-production.up.railway.app/cita`)
     dispatch({
         type: GET_ALL_CITAS,
         payload: citas
@@ -24,14 +24,14 @@ export const allCitas = () => async (dispatch) => {
 }
 
 export const citaPerId = (citaId) => async (dispatch) => {
-    const { cita } = await axios.get(`/citas/${citaId}`);
+    const { cita } = await axios.get(`https://consultorio-production.up.railway.app/cita/${citaId}`);
     dispatch({
       type: GET_CITA_ID,
       payload: cita,
     });
 }
 export const citasPerCLiente = (clienteId) => async (dispatch) => {
-  const { citas } = await axios.get(`http://localhost:3001/cita/cliente/${clienteId}`);
+  const { citas } = await axios.get(`https://consultorio-production.up.railway.app/${clienteId}`);
   dispatch({
     type: GET_CITAS_PER_CLIENT,
     payload: citas,
