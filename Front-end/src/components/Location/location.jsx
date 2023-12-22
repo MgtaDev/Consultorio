1c93ffbb4166c903609dd8f65d28f2b63570ab2d
MgtaDev <passantinodev@gmail.com>
2023-12-22 01:37:49 -0400

    d

--- Front-end/src/components/Location/location.jsx
+++ Front-end/src/components/Location/location.jsx
@@ -7,7 +7,7 @@ const Loaction = () => {
       <div className="max-w-5xl mt-10 mx-auto text-center mb-12">
             <h2 className="text-3xl font-bold tracking-wide uppercase text-gray-800">Donde encontrarnos?</h2>
           </div>
-  <h2 className="text-lg text-gray-600 flex justify-center py-10 ">Disponible proximamente</h2>
+  <h2 className="text-lg text-gray-600 flex justify-center py-10 ">.Disponible proximamente</h2>
 
     </div>
   )
